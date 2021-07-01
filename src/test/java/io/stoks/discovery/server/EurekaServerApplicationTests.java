d337c438c5cb01cab19c5210a12d6001c35bad1b
Salva Vilarrasa <shagy69@gmail.com>
2021-07-01 17:43:15 +0800

    Initial commit

---
+++ src/test/java/io/stoks/discovery/server/EurekaServerApplicationTests.java
@@ -0,0 +1,13 @@
+package io.stoks.discovery.server;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class EurekaServerApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
